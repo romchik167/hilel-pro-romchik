85837df053dc69b96abd6fcf74ce80ef627cd907
romchik167 <lavrenchukroman10@gmail.com>
2025-08-04 12:06:46 +0300

    add homework

--- homework-5/lesson-homework/homework-2/script.js
+++ homework-5/lesson-homework/homework-2/script.js
@@ -1,6 +1,6 @@
 let dollor = 42;
 for(let i = 10; i <= 100; i += 10){
-    let value = 42 * i;
+    let value = dollor * i;
     document.write(value)
     if (i < 100) {
         document.write(", ");
