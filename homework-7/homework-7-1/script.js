0f4562d64f856c337de1294caa3977816d763547
romchik167 <lavrenchukroman10@gmail.com>
2025-08-13 20:45:45 +0300

    changed homework

--- homework-7/homework-7-1/script.js
+++ homework-7/homework-7-1/script.js
@@ -7,7 +7,7 @@ function createSum(){
 }
 const sum = createSum();
 
-console.log(sum(4));  // 4
-console.log(sum(6));  // 10
-console.log(sum(10)); // 20
-console.log(sum(7));  // 27
+console.log(sum(4));
+console.log(sum(6));
+console.log(sum(10));
+console.log(sum(7)); 
