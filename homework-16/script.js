574b396d5bbdd48fde2436237b8b1c4f78ea3684
romchik167 <lavrenchukroman10@gmail.com>
2025-09-17 22:04:42 +0300

    add homework

--- homework-16/script.js
+++ homework-16/script.js
@@ -10,22 +10,60 @@ function Student(firstName, lastName, birthYear) {
     this.attendance = new Array(25);
 
     this.present = function () {
-        const index = this
-            .attendance
-            .indexOf(undefined);
-        if (index !== -1) {
-            this.attendance[index] = true;
+        for (let i = 0; i < this.attendance.length; i++) {
+            if (this.attendance.length <= 25) {
+                if (!(i in this.attendance)) {
+                    this.attendance[i] = true;
+                    return
+                }
+            } else {
+                console.log('В масиві може бути не більше 25 записів')
+            }
         }
     }
     this.absent = function () {
-        const index = this
-            .attendance
-            .indexOf(undefined);
-        if (index !== -1) {
-            this.attendance[index] = false;
+        for (let i = 0; i < this.attendance.length; i++) {
+            if (this.attendance.length <= 25) {
+                if (!(i in this.attendance)) {
+                    this.attendance[i] = false;
+                    return
+                }
+            } else {
+                console.log('В масиві може бути не більше 25 записів')
+            }
         }
+    };
+
+    this.addGrade = function (num) {
+        if (num < 0 || num > 100) {
+            console.log('Оцінка має бути від 0 до 100');
+            return;
+        }
+        for (let i = 0; i < this.grades.length; i++) {
+            if (!(i in this.grades)) {
+                this.grades[i] = num;
+                return;
+            }
+        }
+        console.log('Всі комірки для оцінок заповнені');
+    };
+    let avarageGrade = 0;
+    this.getAverageGrade = function () {
+        let summ = 0;
+        let markedCount = 0;
+        for (let i = 0; i < this.grades.length; i++) {
+            if (i in this.grades && typeof this.grades[i] === 'number') {
+                summ += this.grades[i];
+                markedCount++;
+            }
+        }
+        if (markedCount === 0) 
+            return 0;
+        avarageGrade = (summ / markedCount).toFixed(0);
+        return avarageGrade;
     }
-    this.getAvarageAttendence = function () {
+    let attendanceStudent = 0;
+    this.attendance = function () {
         let presentCount = 0;
         let markedCount = 0;
         for (let i = 0; i < this.attendance.length; i++) {
@@ -36,24 +74,73 @@ function Student(firstName, lastName, birthYear) {
                 markedCount++;
             }
         }
+
         if (markedCount === 0) {
-            return 0
+            attendanceStudent = 0
+        } else {
+            attendanceStudent = (presentCount / markedCount).toFixed(2)
         }
-        return presentCount / markedCount
-
+        return attendanceStudent;
     }
-
+    this.rankingStudent = function () {
+        const avgGrade = this.getAverageGrade();
+        const avgAttendance = this.attendance();
+        if (avgGrade > 90 && avgAttendance > 0.9) {
+            console.log('Молодець');
+        } else if (avgGrade > 90 || avgAttendance > 0.9) {
+            console.log('Добре');
+        } else {
+            console.log('Редиска');
+        }
+    };
 }
 
 const student1 = new Student('Roma', 'Lavrenchuk', 2009)
 console.log(student1)
 console.log(student1.getAge())
-student1.absent();
-student1.absent();
-student1.absent();
+
+student1.absent(); 
+student1.absent(); 
 student1.absent();
 student1.present();
 student1.present();
 student1.present();
 student1.present();
-console.log(student1.getAvarageAttendence())
+
+student1.addGrade(90);
+student1.addGrade(100);
+student1.addGrade(90);
+
+console.log(student1.getAverageGrade())
+student1.rankingStudent();
+
+const student2 = new Student('Ivan', 'Kot', 1987)
+console.log(student2)
+console.log(student2.getAge())
+
+student2.present();
+student2.present();
+student2.present();
+student2.present();
+
+student2.addGrade(50);
+student2.addGrade(100);
+student2.addGrade(50);
+
+console.log(student2.getAverageGrade())
+student2.rankingStudent();
+
+const student3 = new Student('Makar', 'Buhaienko', 2012)
+console.log(student3)
+console.log(student3.getAge())
+
+student3.absent(); 
+student3.absent(); 
+student3.absent();
+
+student3.addGrade(80);
+student3.addGrade(10);
+student3.addGrade(90);
+
+console.log(student3.getAverageGrade())
+student3.rankingStudent();
